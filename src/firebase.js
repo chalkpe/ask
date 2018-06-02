51481e1ff1bfe3a74a3895d78c21ffd977d06bbe
Chalk <chalk@chalk.pe>
2018-06-02 22:42:05 +0900

    parcel hates dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,8 @@
-import dotenv from 'dotenv'
+import config from './config'
 
 import Vue from 'vue'
 import VueFire from 'vuefire'
 import firebase from 'firebase'
 
-dotenv.config()
 Vue.use(VueFire)
-
-firebase.initializeApp({
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
-})
+firebase.initializeApp(config)
