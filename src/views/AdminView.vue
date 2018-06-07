820e3cf8a8cb740bcf495a8d12bfa8015384b8f3
Chalk <chalk@chalk.pe>
2018-06-07 15:57:17 +0900

    i dunno why this works

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -60,11 +60,10 @@ export default {
   }),
 
   async created () {
-    auth.onAuthStateChanged(user => (this.user = user))
+    auth.onAuthStateChanged(user => this.updateToken(this.user = user))
     messaging.requestPermission()
-      .then(() => this.updateToken())
       .then(() => messaging.onTokenRefresh(() => this.updateToken()))
-      .catch(err => console.error('failed to request permission', err))
+      .catch(err => console.error('failed to initialize messaging', err))
   },
 
   methods: {
